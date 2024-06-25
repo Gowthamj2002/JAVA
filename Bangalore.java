5f150d8b569b232e114606a5ab916930e2aaed98
unknown <gowthamj.xworkz@gmail.com>
2024-06-25 10:06:56 +0530

    adding new file

---
+++ Bangalore.java
@@ -0,0 +1,11 @@
+public class Bangalore{
+  public static void main(String args[]){
+  System.out.println("City=Bangalore");
+  System.out.println("Bangalore is a silicon city");
+  System.out.println("Bangalore is a capital city of karnataka");
+  System.out.println("Bangalore is a IT hub");
+}
+
+
+
+}
