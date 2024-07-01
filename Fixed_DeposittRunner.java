3deacffc3b7a81c24bedb04a9300c4769af02993
unknown <gowthamj.xworkz@gmail.com>
2024-07-02 00:46:09 +0530

    jul_2

---
+++ Fixed_DeposittRunner.java
@@ -0,0 +1,34 @@
+public class public class Gold{
+	public static final double gst = 0.10;
+public static void goldRate(){
+int goldRate =7000;
+System.out.println("GOLD RATE PER GRAM :"+goldRate);
+}
+public static void carate(){
+String carate ="24 CARATE";
+System.out.println("GOLD CARATE :"+carate);
+}
+public static void Price(){
+int Price =56000;
+System.out.println("PRICE FOR 8 GRAMS :"+Price);
+
+}
+public static void getGst(int price){
+double getGst =price*Cloth.gst;
+System.out.println("PLUS GST 10% :"+getGst);
+
+}
+public static void totalPrice(int price){
+double totalPrice =price+(price*Gold.gst);
+System.out.println("TOTAL PRICE INCLUDING GST :"+totalPrice);
+}
+}{
+public static void main(String[] args){
+Fixed_Deposit.bankName();
+Fixed_Deposit.bankHolderName();
+Fixed_Deposit.depositAmount();
+Fixed_Deposit.intrest();
+Fixed_Deposit.monthlyProfit(100000, 0.07d);
+
+}
+}
---
+++ Subjects.java
@@ -0,0 +1,8 @@
+class Subjects{
+public static String dsd="eassy";
+public static String dsp="difficult";
+public static String iot="medium";
+public static String cn="medium";
+public static String radar="eassy";
+
+}
